7d47f6ed317582e606255624322bee063f55792f
Vynn <147434242+vynnhere@users.noreply.github.com>
2024-08-02 11:08:55 +0300

    Create index.js
    sigma javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+const house = document.getElementById("house")
+const contactButton = document.getElementById("contactButton")
+
+contactButton.onclick = function() {
+    house.innerHTML += "<p>Discord: vynnhere</p>"
+}
